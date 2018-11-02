539ed4466fc20aa179db6909ead7d014395557fa
Aldric Giacomoni <Trevoke@users.noreply.github.com>
2018-11-02 10:41:45 -0400

    Update lsp-elixir.el

--- lsp-elixir.el
+++ lsp-elixir.el
@@ -1,3 +1,31 @@
+;;; lsp-elixir.el --- Elixir tooling integration into Emacs
+
+;; Copyright © 2018 Aldric Giacomoni
+;;
+;; Author: Aldric Giacomoni <trevoke@gmail.com>
+;; Maintainer: Aldric Giacomoni <trevoke@gmail.com>
+;; URL: http://www.github.com/trevoke/lsp-elixir.el
+;; Version: 0.1.0
+;; Package-Requires: ((lsp-mode "5.0") (emacs "24.4") (company "0.8.0"))
+;; Keywords: languages, elixir, elixirc, mix, hex, alchemist
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'lsp-imenu)
 (require 'lsp-mode)
 
